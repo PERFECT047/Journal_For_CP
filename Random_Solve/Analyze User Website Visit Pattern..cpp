bf594d7897cd3c0a738987610082f530c1b25bd9
PERFECT047 <siddhartha.das.min19@itbhu.ac.in>
2021-06-16 21:53:27 +0530

    Amazon Popular Problems Solve

---
+++ Random_Solve/Analyze User Website Visit Pattern..cpp
@@ -0,0 +1,138 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+ 
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef stringstream ss;
+typedef map<int, int> mii;
+typedef map<ll, int> mli;
+typedef map<char, int> mci;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> mivi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef set<int> seti;
+typedef set<ll> setl;
+typedef set<char> setc;
+typedef set<string> sets;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef map<int, int>::iterator miit;
+typedef map<ll, ll>::iterator mllit;
+typedef map<char, int>::iterator mciit;
+typedef map<string, int>::iterator msiit;
+typedef vector<int>::iterator viit;
+typedef vector<ll>::iterator vlit;
+typedef vector<char>::iterator vcit;
+typedef vector<string>::iterator vsit;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+ 
+void init_code(){
+    fastio();
+    #ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+    #else
+    #define debug(x)
+    #endif 
+}
+ 
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+ 
+
+vector<string> solve(vector<string>& username, vi& timestamp, vector<string>& website) {
+    int n=username.size();
+    unordered_map<string,map<int,string>> data;
+    
+    for(int i=0;i<n;i++)
+        data[username[i]][timestamp[i]]=website[i];
+    map<vector<string>,unordered_map<string,int>> res;
+    
+    for(auto m:data){
+        if(m.ss.size()<3) continue;
+        
+        for(auto i=m.ss.begin();i!=m.ss.end();i++){
+            auto j=i;j++;
+            
+            for(;j!=m.ss.end();j++){
+                auto k=j; k++;
+                
+                for(;k!=m.ss.end();k++){
+                    res[{(*i).ss,(*j).ss,(*k).ss}][m.first]++;
+                }
+            }
+        }
+    }
+    vector<string> s;
+    int ma=0;
+    
+    for(auto i:res){
+        if(i.ss.size()>ma){
+            s=i.ff;
+            ma=i.ss.size();
+        }
+    }
+    return s;
+}
+
+ 
+int main()
+{
+    init_code();
+    
+    vi v(26);
+    string s;
+    cin>>s;
+    for(auto c:s)v[c-'a']++;
+    ll dc=0;
+    for(int i=0;i<26;i++) if(v[i]>0)dc++;
+    
+    
+    return 0;
+}
+
