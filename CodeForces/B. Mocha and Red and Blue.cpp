0c97331555d5c9a0a5167a1fa03640cebfdf14f6
Siddhartha Das <siddhartha.das.min19@itbhu.ac.in>
2021-08-16 19:01:50 +0530

    Some solutions

---
+++ CodeForces/B. Mocha and Red and Blue.cpp
@@ -0,0 +1,141 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+ 
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef stringstream ss;
+typedef map<int, int> mii;
+typedef map<ll, int> mli;
+typedef map<char, int> mci;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> mivi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef set<int> seti;
+typedef set<ll> setl;
+typedef set<char> setc;
+typedef set<string> sets;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef map<int, int>::iterator miit;
+typedef map<ll, ll>::iterator mllit;
+typedef map<char, int>::iterator mciit;
+typedef map<string, int>::iterator msiit;
+typedef vector<int>::iterator viit;
+typedef vector<ll>::iterator vlit;
+typedef vector<char>::iterator vcit;
+typedef vector<string>::iterator vsit;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+ 
+void init_code(){
+    fastio();
+    #ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+    #else
+    #define debug(x)
+    #endif 
+}
+ 
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+void solve() {
+	
+	ll n; 
+	cin >> n;
+	ll arr[n+2];
+	
+	arr[0] = 0;
+	
+	for( ll i = 1; i <= n; i++){
+		char ch;
+		cin >> ch;
+		if(ch == 'R') arr[i] = 0;
+		else if( ch == 'B') arr[i] = 1;
+		else arr[i] = -1;
+	}
+	
+	ll id = 0;
+	
+	for(ll i = 1; i <= n; i++){ 
+		if(arr[i] != -1){
+			id = i; 
+			break;
+		}
+	}
+	
+	ll l = id - 1;
+	
+	while(l > 0){
+		arr[l] = arr[l+1] + 1;
+		l--;
+	}
+	while(id < n){
+		if(arr[id+1] < 0) arr[id+1] = arr[id] + 1;
+		id++;
+	}
+	
+	for(ll i = 1; i <= n; i++){
+		if(arr[i] % 2 == 1) cout << "B";
+		else if(arr[i] % 2 == 0) cout << "R";
+	}
+	cout<< nline;
+}
+ 
+ 
+int main()
+{
+    init_code();
+
+    ll tc = 1;
+    cin>>tc;
+    while(tc--) solve();
+    
+    return 0;
+}
---
+++ CodeForces/D. NP-Hard Problem.cpp
@@ -0,0 +1,166 @@
+#include<bits/stdc++.h>
+ 
+using namespace std;
+ 
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization ("unroll-loops")
+ 
+ 
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+ 
+ 
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+typedef stringstream ss;
+typedef map<int, int> mii;
+typedef map<ll, int> mli;
+typedef map<char, int> mci;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> mivi;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef set<int> seti;
+typedef set<ll> setl;
+typedef set<char> setc;
+typedef set<string> sets;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<char> vc;
+typedef vector<string> vs;
+typedef vector<pii> vpii;
+typedef map<int, int>::iterator miit;
+typedef map<ll, ll>::iterator mllit;
+typedef map<char, int>::iterator mciit;
+typedef map<string, int>::iterator msiit;
+typedef vector<int>::iterator viit;
+typedef vector<ll>::iterator vlit;
+typedef vector<char>::iterator vcit;
+typedef vector<string>::iterator vsit;
+// typedef tree<pair<int, int>, null_type, less<pair<int, int>>, rb_tree_tag, tree_order_statistics_node_update > pbds; // find_by_order, order_of_key
+ 
+ 
+void init_code(){
+    fastio();
+    #ifndef ONLINE_JUDGE
+    #define debug(x) cerr << #x <<" "; _print(x); cerr << nline;
+    #else
+    #define debug(x)
+    #endif 
+}
+ 
+ 
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+
+
+vll edges[100005];
+bool vis[100005] = {0};
+int col[100005] = {-1};
+bool poss = 1;
+
+
+void dfs(ll i, ll icol){
+	if(!vis[i]){
+		col[i] = icol;
+		vis[i] = 1;
+		for(ll side: edges[i]){
+			ll tocol = icol ^ 1;
+			if(!vis[side]){
+				dfs(side, tocol);
+			}
+			else{
+				if(col[side] != tocol) poss = 0;
+			}
+		}
+	}
+	return;
+}
+
+
+void solve(int tc = 0) {
+	
+	ll n, m;
+	
+	cin>> n >> m;
+	
+	for(ll i = 0; i < m; i++){
+		ll u, v;
+		cin>> u >> v;
+		
+		edges[u].pb(v);
+		edges[v].pb(u);
+	}
+	
+	for(ll i = 1; i <= n; i++){
+		dfs(i, 0);
+	}
+	
+	if(poss){
+		ll cnt[2] = {0};
+		for (ll i = 1; i <= n; i++) ++cnt[col[i]];
+		
+		cout << cnt[0] << '\n';
+		for (ll i = 1; i <= n; i++) {
+			if (col[i] == 0) {
+				cout << i << " ";
+			}
+		}
+		cout << '\n';
+		
+		cout << cnt[1] << '\n';
+		for (ll i = 1; i <= n; i++) {
+			if (col[i] == 1) {
+				cout << i << " ";
+			}
+		}
+		cout << nline;
+	}
+	else{
+		cout<<"-1";
+	}
+	
+}
+ 
+ 
+int main()
+{
+	    init_code();
+
+    ll tc = 1;
+    //cin>>tc;
+    while(tc--) solve();
+    
+    return 0;
+}
